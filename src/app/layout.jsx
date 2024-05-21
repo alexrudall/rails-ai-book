09d6054cd78e72835ad65916c4ae2d3683f3f845
Alex Rudall <alexrudall@gmail.com>
2024-05-21 14:11:06 +0100

    Add GA

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,10 @@ import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
 
+import Head from 'next/head'
+import Script from 'next/script'
+const GTM_ID = 'G-FF0VNZ74M4';
+
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
@@ -36,6 +40,22 @@ export default function RootLayout({ children }) {
       className={clsx('h-full antialiased', inter.variable, lexend.variable)}
       suppressHydrationWarning
     >
+      <Head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+            `}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
+      </Head>
       <body className="flex min-h-full bg-white dark:bg-slate-900">
         <Providers>
           <Layout>{children}</Layout>
