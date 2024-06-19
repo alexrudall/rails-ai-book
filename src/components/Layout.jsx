47177b3c95f547c28a95c5e3745702dff22eb4c3
Alex Rudall <alexrudall@gmail.com>
2024-06-19 15:27:56 +0100

    Tweak light mode header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -37,10 +37,10 @@ function Header() {
   return (
     <header
       className={clsx(
-        'sticky top-0 z-50 flex flex-none flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 sm:px-6 lg:px-8 dark:shadow-none',
+        'sticky top-0 z-50 flex flex-none flex-wrap items-center justify-between px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 sm:px-6 lg:px-8 shadow-none',
         isScrolled
-          ? 'dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75'
-          : 'dark:bg-transparent',
+          ? 'bg-slate-900/95 backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75'
+          : 'bg-slate-900',
       )}
     >
       <div className="mr-6 flex lg:hidden">
@@ -48,7 +48,7 @@ function Header() {
       </div>
       <div className="relative flex flex-grow basis-0 items-center">
         <Link href="/" aria-label="Home page">
-          <Logo className="hidden h-9 w-auto fill-slate-700 lg:block dark:fill-sky-100" />
+          <Logo className="hidden h-9 w-auto fill-slate-700 lg:block fill-sky-100" />
         </Link>
       </div>
       <div className="-my-5 mr-6 sm:mr-8 md:mr-0">
@@ -57,7 +57,7 @@ function Header() {
       <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
         <ThemeSelector className="relative z-10" />
         <Link href="https://github.com/alexrudall/railsai" className="group" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
-          <GitHubIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
+          <GitHubIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 group-hover:fill-slate-300" />
         </Link>
       </div>
     </header>
